d8b4ecc7cdc2cb827c6ac8edc4050effc2a8aae5
mk80 <michaelkosko@gmail.com>
2020-09-13 20:09:13 -0700

    first commit

---
+++ satellitebot.py
@@ -0,0 +1,57 @@
+
+import os
+import sys
+import discord
+import re
+
+# get present working dir
+pwd = os.getcwd()
+
+# define file that has the token and store in token var
+token_file = 'token.txt'
+token = ''
+
+with open(token_file,'r') as data:
+  token = data.read().replace('\n','')
+
+# define log file to use
+log_file = pwd + '/' + 'message.log'
+
+# start discord client
+client = discord.Client()
+
+# regex definitions
+bot_name = (r'.*satellitebot.*')
+
+# satellite info
+sat_file =  pwd + '/' + 'ISS.info'
+
+# extra header info
+extra_header_file = pwd + '/' + 'extra.header'
+
+#** max message is 2000 chars; 16 x 125 is the largest message; 16 lines
+
+@client.event  # event decorator/wrapper. More on decorators here: https://pythonprogramming.net/decorators-intermediate-python-tutorial/
+async def on_ready():  # method expected by client. This runs once when connected
+  # logging for later use
+  with open(log_file,'a') as log:
+    log.write(f'We have logged in as {client.user}\n')  # notification of login
+
+@client.event
+async def on_message(message):  # event that happens per any message.
+  # test dm
+  with open(sat_file, 'r') as f:
+    data = f.read()
+
+  with open(extra_header_file, 'r') as h:
+    extra_info = h.read()
+
+  if (message.content.startswith("!test")):
+    # sending as code snippet for proper formatting of text
+    await message.author.send('```' + data + '```')
+    await message.author.send('```' + extra_info + '```')
+
+  found_match = re.match(bot_name,message.content.lower())
+  #if found_match and str(message.author) != "satellitebot#8915":
+
+client.run(token)
