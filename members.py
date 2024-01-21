8c06f5b1c7151cac8eea99b9b1b74db0664ffe84
mk80 <michaelkosko@gmail.com>
2024-01-21 12:20:35 -0500

    building functions

--- members.py
+++ members.py
@@ -5,19 +5,33 @@ import sqlite3
 pwd = os.getcwd()
 
 # build member list
-#   -name   -gps    -satellite list
-db_conn = sqlite3.connect("members.db")
-db_cur = db_conn.cursor()
+#   -name   -gps    -schedule   -satellite list
+#db_conn = sqlite3.connect("members.db")
+#db_cur = db_conn.cursor()
 
 satellites = pwd + '\\satellite.list'
 
 
-def addMember(name, locGPS, sateHolder):
-    pass
+def addMember(name, locGPS, schedule, sateHolder):
+    db_conn = sqlite3.connect("member.db")
+    db_cur = db_conn.cursor()
+
+    result = db_cur.execute("UPSERT ...")
+    db_conn.commit()
+
+    db_conn.close()
+
 
 def listSats():
     with open(satellites, 'r') as f:
         return(f.read())
 
-def addSatToMember():
+def addSatToMember(satellite):
+    db_conn = sqlite3.connect("member.db")
+    db_cur = db_conn.cursor()
+    pass
+
+def updateLocation(locGPS):
+    db_conn = sqlite3.connect("member.db")
+    db_cur = db_conn.cursor()
     pass
